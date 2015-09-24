79639ec6e59237b604b9f41ced1a850550093d76
Robert Wilkinson <wilkinson.robert.a@gmail.com>
2015-09-23 23:29:30 -0700

    Cleans up the client Home component,

--- client/Home.jsx
+++ client/Home.jsx
@@ -3,122 +3,107 @@ var Day = require('./Day.jsx');
 var Match = require('./workArea/Match.jsx');
 var Available = require('./workArea/Available.jsx');
 var Upload = require('./workArea/Upload.jsx');
+
 // var alasql=require('alasql');
 
 var Home = React.createClass({
   getInitialState: function() {
     return {
-      name:null,
+      name: null,
       indexNumber: 0,
-      workNumber:0,
-      matchData:null,
+      workNumber: 0,
+      matchData: null,
       availableData: null,
       loadingIcon: false,
-      previousHeaders: null
+      previousHeaders: null,
     };
   },
-  componentDidMount:function() {
+  componentDidMount: function() {
     this.getAvailableData();
     this.retrieveLastHeaderOrder();
   },
 
-  setStateFunction:function(key, value) {
-    var object={};
-    object[key]=value;
-    this.setState(object);
-  },
-
-  retrieveLastHeaderOrder:function() {
-        
-    var headerObject={School: 'Darden'};
+  retrieveLastHeaderOrder: function() {
+    var headerObject = {School: 'Darden'};
     $.ajax({
       method: 'POST',
       contentType: 'application/json',
       url: '/headerOrder',
       data: JSON.stringify(headerObject),
       success: function(data) {
-        console.log(data,'header data');
-        this.setStateFunction('previousHeaders', data);  
+        this.setState({previousHeaders: data});
       }.bind(this)
     });
   },
 
   setLoadingIcon:function(word) {
-    this.setState({loadingIcon:word});
+    this.setState({loadingIcon: word});
   },
 
-  getAvailableData:function() {
-    var self=this;
+  getAvailableData: function() {
     $.ajax({
       method: 'GET',
       url: '/getAvailableData',
       success: function(data) {
-        console.log("getAvailableData", data[0]);
-        self.setState({availableData:data[0]});
-      }
+        this.setState({availableData: data[0]});
+      }.bind(this)
     });
   },
-
-  exportCSV:function(matchData){
-   var a = document.createElement('a');
-    a.href = 'data:application/csv;charset=utf-8,'+encodeURIComponent(matchData);
+  exportCSV: function(matchData) {
+    var a = document.createElement('a');
+    a.href = 'data:application/csv;charset=utf-8,' + encodeURIComponent(matchData);
     a.target = '_blank';
     a.download = 'match-data.csv';
     document.body.appendChild(a);
     a.click();
   },
-
-  setName:function(name){
-    this.setState({name:name});
+  setName: function(name) {
+    this.setState({name: name});
   },
   setMatchData: function(data) {
-    // console.log('setMatchData fires');
-    this.setState({matchData:data});
+    this.setState({matchData: data});
   },
-
-  setIndexNumber:function(number) {
-    this.setState({indexNumber:number});
+  setIndexNumber: function(number) {
+    this.setState({indexNumber: number});
   },
-  setWorkArea:function(number) {
-    this.setState({workNumber:number});
+  setWorkArea: function(number) {
+    this.setState({workNumber: number});
   },
+  render: function() {
+    var workArea = <div></div>;
+    var workNumber = this.state.workNumber;
+    if (workNumber === 0) {
+      workArea = <Match
+                  workNumber={this.state.workNumber}
+                  setWorkArea={this.setWorkArea}
+                  indexNumber={this.state.indexNumber}
+                  setMatchData={this.setMatchData}
+                  exportCSV={this.exportCSV}
+                  matchData={this.state.matchData}
+                  loadingIcon={this.setLoadingIcon} />;
+    } else if (workNumber === 1) {
+      workArea = <Available
+                  workNumber={this.state.workNumber}
+                  setWorkArea={this.setWorkArea}
+                  indexNumber={this.state.indexNumber}
+                  availableData={this.state.availableData}/>;
+    } else if (workNumber === 2) {
+      workArea = <Upload
+                  workNumber={this.state.workNumber}
+                  setWorkArea={this.setWorkArea}
+                  indexNumber={this.state.indexNumber}
+                  previousHeaders={this.state.previousHeaders} />;
+    }
 
-  render: function(){
-    console.log('previousHeaders',this.state.previousHeaders);
-    // console.log(this.state.matchData,'matchData');
-  // console.log('matchData',this.state.matchData);
-  var self=this;
-  var workArea=<div></div>
-  var workNumber=this.state.workNumber;
-  if(workNumber===0) {
-    workArea=<Match workNumber={this.state.workNumber}
-    setWorkArea={this.setWorkArea}
-    indexNumber={this.state.indexNumber}
-    setMatchData={this.setMatchData}
-    exportCSV={this.exportCSV}
-    matchData={this.state.matchData}
-    loadingIcon={this.setLoadingIcon} />;
-  } else if(workNumber===1) {
-    workArea=<Available workNumber={this.state.workNumber}
-    setWorkArea={this.setWorkArea}
-    indexNumber={this.state.indexNumber} 
-    availableData={this.state.availableData}/>;
-  } else if(workNumber===2) {
-    workArea=<Upload workNumber={this.state.workNumber}
-    setWorkArea={this.setWorkArea}
-    indexNumber={this.state.indexNumber}
-    previousHeaders={this.state.previousHeaders} />;
-  }
-
-    return(
+    return (
       <div>
-        <h1 id="header">MATCHLY</h1>
+        <h1 id='header'>MATCHLY</h1>
         <div id='workArea'>
             {workArea}
         </div>
       </div>
     );
-  }
+  },
 });
 
-module.exports=Home;
+module.exports = Home;
