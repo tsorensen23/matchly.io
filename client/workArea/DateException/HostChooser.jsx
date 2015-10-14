a62bf07ac25cafadc19bcec5ac3d2eb9ec0f436e
formula1 <samtobia@gmail.com>
2015-10-13 18:01:06 -0700

    max height

--- client/workArea/DateException/HostChooser.jsx
+++ client/workArea/DateException/HostChooser.jsx
@@ -56,46 +56,48 @@ var AddExceptionDay = React.createClass({
     return (
         <div>
         <h2> Now select the hosts who will be available on that date</h2>
-      <table className='table table-condensed'>{this.state.hosts.map(function(host) {
-        var isAvailable = -1 === this.findIndex(host.MatchInfo.exceptionDate, this.props.date);
-        if (!host.MatchInfo.matchDates) {
-          host.MatchInfo.matchDates = host.MatchInfo.matches.map(function(match) {
-            return match.date;
-          });
-        }
+        <div style={{height: '500px', overflowY: 'scroll'}}>
+          <table className='table table-condensed'>{this.state.hosts.map(function(host) {
+            var isAvailable = -1 === this.findIndex(host.MatchInfo.exceptionDate, this.props.date);
+            if (!host.MatchInfo.matchDates) {
+              host.MatchInfo.matchDates = host.MatchInfo.matches.map(function(match) {
+                return match.date;
+              });
+            }
 
-        if (host.MatchInfo.matches.length) console.log('matches', host.MatchInfo.matches);
+            if (host.MatchInfo.matches.length) console.log('matches', host.MatchInfo.matches);
 
-        var takenIndex = -1;
-        var isTaken = false;
-        if (isAvailable) {
-          takenIndex = this.findIndex(host.MatchInfo.matchDates, this.props.date);
-          isTaken = -1 !== takenIndex;
-        }
+            var takenIndex = -1;
+            var isTaken = false;
+            if (isAvailable) {
+              takenIndex = this.findIndex(host.MatchInfo.matchDates, this.props.date);
+              isTaken = -1 !== takenIndex;
+            }
 
-        var onoff = isAvailable;
+            var onoff = isAvailable;
 
-        var contact = host.Contact;
-        return (
-          <tr style={{
-              backgroundColor: !isAvailable ? '#E26A6A' : isTaken ? 'yellow' : '#2ecc71',
-              fontSize: '1.1em'
-            }}>
-            <td style={{verticalAlign: 'center'}}><span className={`glyphicon ${isAvailable ? 'glyphicon-ok' : 'glyphicon glyphicon-remove'}`} /></td>
-            <td style={{verticalAlign: 'center'}}><b>{contact.First}</b></td>
-            <td style={{verticalAlign: 'center'}}><b> {contact.Last}</b></td>
-            <td style={{verticalAlign: 'center'}}><b>{
-              isTaken ? 'Has Visitor' : isAvailable ? 'Is Available' : 'Is Unavailable'
-            }</b></td>
-            <td style={{textAlign: 'right', verticalAlign: 'center'}}>
-              <button className={`btn`}  style={{width: '200', padding: '4'}} onClick={toggleHost.bind(this, host, onoff)}>
-                {onoff ? 'Make Unavailable' : 'Make Available'} on Date
-              </button>
-            </td>
-          </tr>
-        );
-      }.bind(this))}
-      </table>
+            var contact = host.Contact;
+            return (
+              <tr style={{
+                  backgroundColor: !isAvailable ? '#E26A6A' : isTaken ? 'yellow' : '#2ecc71',
+                  fontSize: '1.1em'
+                }}>
+                <td style={{verticalAlign: 'center'}}><span className={`glyphicon ${isAvailable ? 'glyphicon-ok' : 'glyphicon glyphicon-remove'}`} /></td>
+                <td style={{verticalAlign: 'center'}}><b>{contact.First}</b></td>
+                <td style={{verticalAlign: 'center'}}><b> {contact.Last}</b></td>
+                <td style={{verticalAlign: 'center'}}><b>{
+                  isTaken ? 'Has Visitor' : isAvailable ? 'Is Available' : 'Is Unavailable'
+                }</b></td>
+                <td style={{textAlign: 'right', verticalAlign: 'center'}}>
+                  <button className={`btn`}  style={{width: '200', padding: '4'}} onClick={toggleHost.bind(this, host, onoff)}>
+                    {onoff ? 'Make Unavailable' : 'Make Available'} on Date
+                  </button>
+                </td>
+              </tr>
+            );
+          }.bind(this))}
+          </table>
+        </div>
       </div>
     );
   }
