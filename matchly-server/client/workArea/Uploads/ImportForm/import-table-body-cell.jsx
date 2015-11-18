d09f98614416b8d78dcf5e2bdb434bd3df62bee7
Robert Wilkinson <wilkinson.robert.a@gmail.com>
2015-11-17 18:26:31 -0800

    Table cells colors adjust properly when you set an employer alis

--- matchly-server/client/workArea/Uploads/ImportForm/import-table-body-cell.jsx
+++ matchly-server/client/workArea/Uploads/ImportForm/import-table-body-cell.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 var green = 'green';
 var red = 'red';
 var yellow = 'yellow';
+var white = 'white';
 
 class ImportTableBodyCell extends React.Component{
   constructor(props){
@@ -14,30 +15,42 @@ class ImportTableBodyCell extends React.Component{
       backgroundColor: 'white'
     };
     this.props.store.on('set-headers', function() {
-      console.log(this.props.store.possibleEmployers);
+var value;
       if(Object.keys(this.props.store.possibleEmployers).indexOf(this.props.visitor[this.state.key]) !== -1) {
         var passedEmployer = this.props.visitor[this.state.key];
-        var value = this.props.store.possibleEmployers[passedEmployer];
+        value = this.props.store.possibleEmployers[passedEmployer];
+         if(typeof value == "object") {
+           this.setState({backgroundColor: red});
+         }
          if(Array.isArray(value)) {
            this.setState({backgroundColor: yellow});
          }
-         if(value === null) {
-           this.setState({backgroundColor: red});
-         } else {
+         if( typeof value === 'string') {
            this.setState({backgroundColor: green});
-         }
+         }      
       }
       if (Object.keys(this.props.store.possibleSchools).indexOf(this.props.visitor[this.state.key]) !== -1) {
         var passedSchool = this.props.visitor[this.state.key];
-        var value = this.props.store.possibleSchools[passedSchool];
+        value = this.props.store.possibleSchools[passedSchool];
+        if (typeof value == "object") {
+          this.setState({backgroundColor: red});
+        }
         if (Array.isArray(value)) {
           this.setState({backgroundColor: yellow});
         }
-        if (value === null) {
-          this.setState({backgroundColor: red});
-        } else {
-          this.setState({backgroundColor: green});
-        }
+       if (typeof value === 'string') {
+         this.setState({backgroundColor: green});
+         }      
+      }
+      var values = Object.keys(this.props.store.possibleEmployers).map(function(key){
+        return this.props.store.possibleEmployers[key];
+      }.bind(this));
+
+      Object.keys(this.props.store.possibleSchools).forEach(function(key){
+              values.push(this.props.store.possibleSchools[key]);
+      }.bind(this));
+      if (values.indexOf(this.props.visitor[this.state.key]) !== -1) {
+        this.setState({backgroundColor: green});
       }
     }.bind(this));
     this.props.collumnStore.on('is-school', function(e) {
@@ -49,35 +62,6 @@ class ImportTableBodyCell extends React.Component{
       this.setState({
         employer : true
       });
-    }.bind(this));
-    this.props.store.on('visitor-update', function() {
-      if(Object.keys(this.props.store.possibleEmployers).indexOf(this.props.visitor[this.state.key]) !== -1) {
-        var passedEmployer = this.props.visitor[this.state.key];
-        var value = this.props.store.possibleEmployers[passedEmployer];
-         if(Array.isArray(value)) {
-           this.setState({backgroundColor: yellow});
-         }
-         if(value === null) {
-           console.log(this.props.store.possibleEmployers)
-           console.log(value);
-           console.log(passedEmployer);
-           this.setState({backgroundColor: red});
-         } else {
-           this.setState({backgroundColor: green});
-         }
-      }
-      if (Object.keys(this.props.store.possibleSchools).indexOf(this.props.visitor[this.state.key]) !== -1) {
-        var passedSchool = this.props.visitor[this.state.key];
-        var value = this.props.store.possibleSchools[passedSchool];
-        if (Array.isArray(value)) {
-          this.setState({backgroundColor: yellow});
-        }
-        if (value === null) {
-          this.setState({backgroundColor: red});
-        } else {
-          this.setState({backgroundColor: green});
-        }
-      }
     }.bind(this));
   }
   fireModal() {
