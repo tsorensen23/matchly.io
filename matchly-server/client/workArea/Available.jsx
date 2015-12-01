ad04c95aa1953271f122d4dc6570bd2abe7f1dda
rouzbeh84 <rouzbeh.sarrafieh@gmail.com>
2015-11-30 17:17:47 -0800

    Matching page styles have been heavily updated and new navigation bar as well. Cleaned up a lot of unused styles from main.css

--- matchly-server/client/workArea/Available.jsx
+++ matchly-server/client/workArea/Available.jsx
@@ -2,6 +2,7 @@ var React = require('react');
 var SECTIONS = ['A', 'B', 'C', 'D', 'E'];
 var TIMES = ['8:00', '10:00', '11:45'];
 var MyEE = require('../Stores/AvailabilityStore');
+
 var Available = React.createClass({
   componentDidMount: function() {
     this.myEE = new MyEE();
@@ -26,12 +27,9 @@ var Available = React.createClass({
   render:function() {
     var _this = this;
     return (
-      <div id='classAvailable'>
+      <div className='classAvailable'>
         <form onSubmit={this.sendClassConstraints}>
-          <table>
-              <div id='topRow'>
-              <tr>
-                <div id='topRowTitles'>
+                <div className='topRowTitles'>
                   {
                     [<h3 className='sections'></h3>]
                     .concat(SECTIONS.map(function(letter) {
@@ -39,25 +37,23 @@ var Available = React.createClass({
                     }))
                   }
                 </div>
-              </tr>
-              </div>
-            <br></br>
             {TIMES.map(function(time, i) {
-              return (<tr>
-                <h3 className='row-title sections'>{time}</h3>
-                {SECTIONS.map(function(letter) {
-                  var cur = letter + (i + 1);
-                  return (<input required='true'
-                    type='number'
-                    className={cur + ' sections'}
-                    ref={cur}
-                    onChange= {_this.changeHandler.bind(_this, cur)}
-                    value={_this.state.availableData ? _this.state.availableData[cur].availableSpots : 0}
-                  />);
-                })}
-              </tr>);
+              return (
+                <div>
+                  <h3 className='row-title sections'>{time}</h3>
+                  {SECTIONS.map(function(letter) {
+                    var cur = letter + (i + 1);
+                    return (<input required='true'
+                      type='number'
+                      className={cur + ' sections'}
+                      ref={cur}
+                      onChange= {_this.changeHandler.bind(_this, cur)}
+                      value={_this.state.availableData ? _this.state.availableData[cur].availableSpots : 0}
+                    />);
+                  })}
+                </div>
+              );
             })}
-          </table>
           <input id='updateButton' type='submit' value='Update'></input>
         </form>
       </div>
--- matchly-server/client/workArea/Match/Match.jsx
+++ matchly-server/client/workArea/Match/Match.jsx
@@ -64,13 +64,16 @@ var Match = React.createClass({
 
           </div>
           <HostChooser date={this.props.date} />
-          <ProgressButton ref='button' onClick={this.match}>MATCH</ProgressButton>
-          <button id='exportButton' onClick={this.exportToCSV}>Export Data to CSV File</button>
+          <ProgressButton className="btn btn-primary" ref='button' onClick={this.match}>MATCH</ProgressButton>
+          <div className="text-center" style={{marginTop: '15'}}>
+
+          <button className="btn btn-info" onClick={this.exportToCSV}>Export Data to CSV File</button>
+          </div>
         </div>
         <div id='loading'>
         </div>
         <div id='data'>
-          <table className='table table-condensed minorPadding'>
+          <table className='table table-condensed'>
             <thead>
               <tr>
                 <th>Visitor First Name</th>
