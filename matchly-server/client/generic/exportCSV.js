caa7dcb37e4f28e69db256b5a6c5cba467a5a034
Robert Wilkinson <wilkinson.robert.a@gmail.com>
2015-11-30 22:13:34 -0800

    parse works

--- matchly-server/client/generic/exportCSV.js
+++ matchly-server/client/generic/exportCSV.js
@@ -1,6 +1,7 @@
+var Baby = require('babyparse');
 module.exports = function(name, data) {
   var a = document.createElement('a');
-  a.href = 'data:application/csv;charset=utf-8,' + encodeURIComponent(Papa.unparse(data));
+  a.href = 'data:application/csv;charset=utf-8,' + encodeURIComponent(Baby.unparse(data));
   a.target = '_blank';
   a.download = name + '.csv';
   document.body.appendChild(a);
