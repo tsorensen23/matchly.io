bc4aba92eac64c9dde6cb05ae206b740336f88db
Robert Wilkinson <wilkinson.robert.a@gmail.com>
2015-12-04 19:03:14 -0800

    logs errors, converts tabs to spaces

--- matchly-server/server/controllers/userController.js
+++ matchly-server/server/controllers/userController.js
@@ -44,19 +44,24 @@ module.exports = {
     var startDate = moment.utc(req.body.date).subtract(1, 'minute').toDate();
     var endDate = moment.utc(req.body.date).add(1, 'minute').toDate();
     // var date = new Date("2015-11-30T08:00:00.000Z");
+    console.log(startDate, endDate);
     db.Host.find( {}, function ( err, data ) {
-    	data.forEach( function ( host ) {
-    		if( host.MatchInfo.matches ) {
-    			for( var i = 0; i < host.MatchInfo.matches.length; i++ ) {
-    				if( ( Date.parse( host.MatchInfo.matches[ i ].date ) > startDate ) && (
-    						(Date.parse(host.MatchInfo.matches[ i ].date)) <
-    						endDate ) ) {
-    					host.MatchInfo.matches.splice( i, 1 );
-    					host.save();
-    				}
-    			}
-    		}
-    	} );
+      if(err) {
+        next(err);
+      }
+      data.forEach( function ( host ) {
+        if( host.MatchInfo.matches ) {
+          for( var i = 0; i < host.MatchInfo.matches.length; i++ ) {
+            if( ( Date.parse( host.MatchInfo.matches[ i ].date ) > startDate ) && (
+                  (Date.parse(host.MatchInfo.matches[ i ].date)) <
+                  endDate ) ) {
+              console.log(host);
+              host.MatchInfo.matches.splice( i, 1 );
+              host.save();
+            }
+          }
+        }
+      } );
     } );
     Visitors.find({'MatchInfo.visitDate': { $lte: endDate, $gte: startDate}}).remove(function(err, data){
 
