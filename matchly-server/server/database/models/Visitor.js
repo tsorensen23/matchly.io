77b52ccff98d892dedbf1597ea65091bb3917ac0
Robert Wilkinson <wilkinson.robert.a@gmail.com>
2015-12-05 14:59:23 -0800

    adds semicolon, presave hook to check if region is in US

--- matchly-server/server/database/models/Visitor.js
+++ matchly-server/server/database/models/Visitor.js
@@ -3,14 +3,16 @@ var Match = require('./../../../matchingAlgorithm/Match.js');
 var db = require('./../connection');
 
 var Schema = mongoose.Schema;
+const STATES= ["AL","AK","AZ","AR","CA","CO","CT","DE","DC","FL","GA","HI","ID","IL","IN","IA","KS","KY","LA","ME","MD","MA","MI","MN","MS","MO","MT","NE","NV","NH","NJ","NM","NY","NC","ND","OH","OK","OR","PA","RI","SC","SD","TN","TX","UT","VT","VA","WA","WV","WI","WY"]
+
 
 var visitorSchema = new Schema({
   Characteristics: {
     Military: {type: String, default: ' '  },
-    Country: {type: String, default: 'United States'},
-    Citizenship: {type: String, default: ' '  },
-    Undergrad: {type: String, default: ' '  },
-    Employer: {type: String, default: ' '  },
+    Country: {type: String},
+    Citizenship: {type: String, required: true  },
+    Undergrad: {type: String, required: true  },
+    Employer: {type: String, required: true  },
     Industry: {type: String, default: 'Business' },
     City: {type: String, default: ' '  },
     State: {type: String, default: ' '  },
@@ -32,6 +34,13 @@ var visitorSchema = new Schema({
     }
   },
   schoolCode: {type: Schema.Types.ObjectId, ref: 'School'}
+});
+
+visitorSchema.pre('save', function(next) {
+  if(STATES.indexOf(this.Characteristics.State) > -1){
+    this.Characteristics.Country = 'United States';
+  }
+  next();
 });
 
 // var virtual = visitorSchema.virtual('match');
