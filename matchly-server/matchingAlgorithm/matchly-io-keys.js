0a7738fe2717cae0b1224b15dfccad41afd2693e
rouzbeh84 <rouzbeh.sarrafieh@gmail.com>
2015-12-05 14:40:05 -0800

    cleaned up order of scoring to be prioritized and algorithm3 formatting a bit

--- matchly-server/matchingAlgorithm/matchly-io-keys.js
+++ matchly-server/matchingAlgorithm/matchly-io-keys.js
@@ -1,12 +1,12 @@
 var MatchKey = require('./MatchKey');
 
 module.exports = [
-  new MatchKey('Gender',100000),
   new MatchKey('State',1),
   new MatchKey('City',10),
   new MatchKey('Industry',100),
   new MatchKey('Employer',1000),
   new MatchKey('Undergrad',10000),
+  new MatchKey('Gender',100000),
   new MatchKey('Citizenship',1000000),
   new MatchKey('Country',10000000),
   new MatchKey('Military',100000000),
