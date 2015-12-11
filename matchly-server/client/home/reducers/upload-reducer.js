a2a2a391b8444d305f392cfcaf891d50ae3c86d5
Robert Wilkinson <wilkinson.robert.a@gmail.com>
2015-12-11 11:29:30 -0800

    upload reducer

---
+++ matchly-server/client/home/reducers/upload-reducer.js
@@ -0,0 +1,10 @@
+export default function upload(state = false, action){
+  switch(action.type){
+    case 'START_UPLOAD':
+      return true;
+    case 'SUCCESS_DATA_UPLOAD':
+      return false;
+    default:
+      return false;
+  }
+}
