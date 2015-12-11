c3b47dd0799429d6d63105f95494e338ce5219b7
Robert Wilkinson <wilkinson.robert.a@gmail.com>
2015-12-10 21:31:29 -0800

    Moved reducers into seperate files and used combinereducers

--- matchly-server/client/home/reducers.js
+++ matchly-server/client/home/reducers.js
@@ -1,8 +1,11 @@
+import { combineReducers } from 'redux';
+import { syncReduxAndRouter, routeReducer } from 'redux-simple-router';
+
 import matches from './reducers/match-reducer';
 import visitors from './reducers/visitors-reducer';
 import hosts from './reducers/hosts-reducer';
-import employers from './reducers/employers-reducer';
-import schools from './reducers/schools-reducer';
+import allEmployers from './reducers/employers-reducer';
+import allSchools from './reducers/schools-reducer';
 import finished from './reducers/finished-reducer';
 import data from  './reducers/data-reducer';
 import headers from './reducers/headers-reducer';
@@ -24,49 +27,17 @@ function upload(state = false, action){
 
 
 
-function wholeState(state = {}, action) {
-  switch(action.type) {
-    case 'FINISH_HEADER_MATCH':
-      var dataArray = state.data;
-      var newDataArray = [];
-      for (var i = 0; i < dataArray[0].data.length; i++) {
-         var dataObject = {};
-         newDataArray.push(dataObject);
-      }
-      dataArray.reduce(function(prev, curr) {
-        return curr.data.map(function(dp, index){
-          if(dp) {
-          prev[index][curr.key] = dp.trim();
-          } else {
-            prev[index][curr.key] = ' ';
-          }
-          return prev[index]
-        });
-      }, newDataArray)
-      return Object.assign({}, state, {
-        finished: newDataArray
-      });
-    default:
-      return state;
-  }
-}
-import { syncReduxAndRouter, routeReducer } from 'redux-simple-router';
-export default function(state = {}, action = {}) {
-  state = wholeState(state, action);
-  return {
-    hostsOrVisitors: hostsOrVisitors(state.hostsOrVisitors, action),
-    upload: upload(state.upload, action),
-    schoolMatches: schoolMatches(state.schoolMatches, action),
-    employerMatches: employerMatches(state.employerMatches, action),
-    headers: headers(state.headers, action),
-    data: data(state.data, action),
-    finished: finished(state.finished, action),
-    allSchools: schools(state.allSchools, action),
-    allEmployers: employers(state.allEmployers, action),
-    hosts: hosts(state.hosts, action),
-    visitors: visitors(state.visitors, action),
-    matches: matches(state.matches, action),
-    routing: routeReducer(state.routing, action),
-    calendar: calendar(state.calendar, action)
-  };
-}
+export default combineReducers({
+  routing: routeReducer,
+  hostsOrVisitors,
+  visitors,
+  matches,
+  calendar,
+  allSchools,
+  allEmployers,
+  data,
+  finished,
+  headers,
+  employerMatches,
+  schoolMatches
+})
--- matchly-server/client/home/reducers/finished-reducer.js
+++ matchly-server/client/home/reducers/finished-reducer.js
@@ -1,5 +1,23 @@
 export default function finished(state = [], action){
   switch(action.type) {
+    case 'FINISH_HEADER_MATCH':
+      var dataArray = action.data;
+      var newDataArray = [];
+      for (var i = 0; i < dataArray[0].data.length; i++) {
+         var dataObject = {};
+         newDataArray.push(dataObject);
+      }
+      dataArray.reduce(function(prev, curr) {
+        return curr.data.map(function(dp, index){
+          if(dp) {
+          prev[index][curr.key] = dp.trim();
+          } else {
+            prev[index][curr.key] = ' ';
+          }
+          return prev[index]
+        });
+      }, newDataArray)
+      return newDataArray;
     case 'CHANGE_VALUE':
       var key = action.key;
       var oldValue = action.oldValue;
