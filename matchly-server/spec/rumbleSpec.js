50884d4c16a4dbe1efb9756c7557e5a6fcc5c10d
Robert Wilkinson <wilkinson.robert.a@gmail.com>
2015-12-14 16:52:30 -0800

    Rumble spec

--- matchly-server/spec/rumbleSpec.js
+++ matchly-server/spec/rumbleSpec.js
@@ -3,66 +3,106 @@
 var rumble = require('../matchingAlgorithm/algorithm3');
 var expect = require('chai').expect;
 var Availability = require('../server/database/models/Availability');
+var Visitor = require('../server/database/db').Visitor;
+var Host = require('../server/database/db').Host;
+var mongoose = require('mongoose');
 
 describe('rumble', function() {
-  var visitors = require('./mocks/visitors.json');
-  var hosts = require('./mocks/hosts.json');
+  afterEach(function(done) {
+    Visitor.find({}).remove().exec();
+    Host.find({}).remove().exec();
+    mongoose.models = {};
+    mongoose.modelSchemas = {};
+    mongoose.connection.close();
+    done();
+  })
+  var visitors = Visitor.create(require('./mocks/visitors.json'));
+  var hosts = Host.create(require('./mocks/hosts.json'));
   var availability = new Availability();
 
   describe('calculate match score', function() {
-    it('calculate match score between the first visitor and host', function() {
-      var matchScore = rumble.calculatematchScore(visitors[0], hosts[0]);
-      expect(matchScore.score).to.eq(111000000);
-      expect(matchScore.count).to.eq(3);
-    });
-  });
-
-  describe('best match score', function() {
-    it('calculate the best match scores between the visitor, hosts and constraint', function() {
-      var vistChars = JSON.parse(JSON.stringify(visitors[0].Characteristics));
-      var hostChars = JSON.parse(JSON.stringify(hosts[1].Characteristics));
-      var match = rumble.getBestMatch(visitors[0], hosts, availability);
-      expect(match.host).to.eq(hosts[1]);
-      expect(match.score).to.eq(111100000);
-      expect(hostChars.Gender === vistChars.Gender).to.eq(match.matchedOn.Gender);
-      expect(hostChars.State === vistChars.State).to.eq(match.matchedOn.State);
-      expect(hostChars.Industry === vistChars.Industry).to.eq(match.matchedOn.Industry);
-      expect(hostChars.Employer === vistChars.Employer).to.eq(match.matchedOn.Employer);
-      expect(hostChars.Undergrad === vistChars.Undergrad).to.eq(match.matchedOn.Undergrad);
-      expect(hostChars.Citizenship === vistChars.Citizenship).to.eq(match.matchedOn.Citizenship);
-      expect(hostChars.Country === vistChars.Country).to.eq(match.matchedOn.Country);
-      expect(hostChars.Military === vistChars.Military).to.eq(match.matchedOn.Military);
-    });
-  });
-
-  describe('validateTotalAvailable', function() {
-    it('should throw error when not enough spaces are available', function() {
-      var availability = new Availability();
-      ['A','B','C','D','E'].forEach(function(letter) {
-        for (var number = 1; number < 4; number++) {
-          availability[letter + number].availableSpots = 0;
+    it('should return 0 when there is no match', function() {
+      let visitor = new Visitor({
+        Characteristics: {
+          Military: 'Veteran',
+          Citizenship: 'USA',
+          Industry: 'Pharmaceuticals'
+        },
+        Contact: {
+          First: 'Rob',
+          Last: 'Wilkinson'
+        }
+      });
+      let host = new Host({
+        Characteristics: {
+          Military: '',
+          Citizenship: ''
+        },
+        Contact: {
+          First: 'Norman',
+          Last: 'Schwatrzkopf'
         }
       });
 
-      expect(
-        rumble.validateTotalAvailable
-        .bind(rumble, visitors, availability)
-      ).to.throw();
+      var matchScore = rumble.calculatematchScore(visitor, host);
+      expect(matchScore.score).to.eq(0);
+      expect(matchScore.count).to.eq(0);
     });
-
-    it('should not throw error when enough spaces are available', function() {
-      var availability = new Availability();
-      ['A','B','C','D','E'].forEach(function(letter) {
-        for (var number = 1; number < 4; number++) {
-          availability[letter + number].availableSpots = 69;
+    it('should return Proper Match Count when there is match', function() {
+      let visitor = new Visitor({
+        Characteristics: {
+          Military: 'Veteran',
+          Citizenship: 'USA'
+        },
+        Contact: {
+          First: 'Rob',
+          Last: 'Wilkinson'
+        }
+      });
+      let host = new Host({
+        Characteristics: {
+          Military: '',
+          Citizenship: 'USA'
+        },
+        Contact: {
+          First: 'Norman',
+          Last: 'Schwatrzkopf'
         }
       });
 
-      expect(
-        rumble.validateTotalAvailable
-        .bind(rumble, visitors, availability)
-      ).to.not.throw();
+      var matchScore = rumble.calculatematchScore(visitor, host);
+      expect(matchScore.count).to.eq(1);
     });
   });
+
+  // describe('validateTotalAvailable', function() {
+  //   it('should throw error when not enough spaces are available', function() {
+  //     var availability = new Availability();
+  //     ['A','B','C','D','E'].forEach(function(letter) {
+  //       for (var number = 1; number < 4; number++) {
+  //         availability[letter + number].availableSpots = 0;
+  //       }
+  //     });
+
+  //     expect(
+  //       rumble.validateTotalAvailable
+  //       .bind(rumble, visitors, availability)
+  //     ).to.throw();
+  //   });
+
+  //   it('should not throw error when enough spaces are available', function() {
+  //     var availability = new Availability();
+  //     ['A','B','C','D','E'].forEach(function(letter) {
+  //       for (var number = 1; number < 4; number++) {
+  //         availability[letter + number].availableSpots = 69;
+  //       }
+  //     });
+
+  //     expect(
+  //       rumble.validateTotalAvailable
+  //       .bind(rumble, visitors, availability)
+  //     ).to.not.throw();
+  //   });
+  // });
 
 });
